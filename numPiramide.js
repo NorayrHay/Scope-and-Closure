b3a3e2666b6f205dd40cfd61df8f6460fbacc2a4
NorayrHay <hayrikyannoro@gmail.com>
2021-12-08 01:37:21 +0400

    feat: i added my file and numPiramide.js  task from last hommework

---
+++ numPiramide.js
@@ -0,0 +1,25 @@
+// function piramide(n){
+//   let arr = [], res = "", iter;
+
+//   for (let i = 1, i2 = n; i <= n; i2++, i++){
+//     res += i;
+//     iter = i2 - i;
+//     arr.unshift(i);
+    
+//    for (let j = 1; j <= i - 1; j++){
+//        arr.unshift(arr[0] + iter--)
+//        res += " " + arr[0];
+//    } 
+//    res += '\n';
+//   }
+//   return res;
+// }
+// console.log(piramide(5));
+
+
+//from one of the last homeworks
+// //1
+// //2 6
+// //3 7 10
+// //4 8 11 13
+// //5 9 12 14 15
